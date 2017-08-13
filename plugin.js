24b855c8fafc73b23208c1118a73f0de7fb60f86
Juri Gagarov <mapman100@yandex.com>
2017-08-13 15:24:05 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,48 +1,14 @@
 {
-  "moduleId" : "mapman100/Here",
-  "title" : "HERE",
-  "subtitle" : "Map from here.com",
-
-  "backButton" : true,
-  "enableGPS" : false,
-  "zoomControl" : false,
-  "screenLockRot" : false,
-  "reload" : true,
-
-  "shouldOverrideUrlLoading1" : "http://m.here.com",
-  "shouldOverrideUrlLoading2" : "https://m.here.com",
-
-  "loadDataWithBaseUrl1" : "https://m.here.com",
-  "loadDataWithBaseUrl3" : "text/html",
-  "loadDataWithBaseUrl4" : "utf-8",
-  "loadDataWithBaseUrl5" : null,
-
-  "browserLaunchLink" : "http://www.here.com",
-
-  "loadDataWithBaseUrl2" :
-  "
-  <script type='text/javascript'>
-
-  // localStorage.clear();
-  document.cookie='USE_MOBILE=false; expires=Fri, 31 Dec 9999 23:59:59 GMT; domain=.here.com;';
-  document.cookie='FORCE_MHERE=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; domain=.here.com;';
-
-  if (localStorage.getItem('mh5-isFirstTime') === null) {
-// First time
-localStorage.setItem('mh5-isFirstTime', true);
-localStorage.setItem('b', true);
-localStorage.setItem('t', true);
-localStorage.setItem('nn', true);
-var newSettings = {'trafficIncidentsEnabled':true,'mapSchema':'normal.day.traffic'};
-localStorage.setItem('settings',JSON.stringify(newSettings));
-}
-
-// Set center and zoom
-var center = {latitude : #lat#, longitude : #lng#};
-localStorage.setItem('1html5-map-center', JSON.stringify(center));
-localStorage.setItem('1html5-map-zoom', '#zoom#');
-window.location.href = 'https://m.here.com/?bmk=1&mord=1#action=search&params=%7B%7D&bmk=1';
-</script>
-"
-
+"moduleId" : "mapman100/Here",
+"title" : "HERE_WEGO",
+"subtitle" : "wego.here.com",
+"backButton" : true,
+"enableGPS" : true,
+"zoomControl" : false,
+"screenLockRot" : false,
+"reload" : true,
+"shouldOverrideUrlLoading1" : "http://wego.here.com",
+"shouldOverrideUrlLoading2" : "https://wego.here.com",
+"loadUrl" : "https://mobile.here.com/?map=#lat#,#lng#,#zoom#,normal&x=ep",
+"browserLaunchLink" : "https://mobile.here.com/?map=#lat#,#lng#,#zoom#,normal&x=ep"
 }
